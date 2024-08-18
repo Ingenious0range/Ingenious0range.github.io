e9926d894815b61c1d5e682edb84b6cd994e8e9d
Lilac <izziemadelyn@gmail.com>
2024-08-17 21:34:58 -0600

    Update index.html, index.html, and 2 more files...

--- cipher/index.js
+++ cipher/index.js
@@ -3,6 +3,8 @@ function load() {
 	document.getElementById("ciphertext-input").value = "";
 	document.getElementById("sub-table-input").value = "";
 	document.getElementById("plaintext-input").value = "";
+	disableBtn("plain");
+	disableBtn("cipher");
 }
 
 function shuffleArray(array) {
@@ -21,19 +23,30 @@ function chanceOfTrue(percentChance) {
 
 const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
-const regularCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", ",", "?", "!", "(", ")", '"', "'", ":", ";", "-", "_", "/", " ", "<", ">"];
-const dupeRegularCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", ",", "?", "!", "(", ")", '"', "'", ":", ";", "-", "_", "/", " ", "<", ">"];
-const specialCharacters = ["á", "é", "í", "ó", "ť"];
+const regularCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", ",", "?", "!", "(", ")", '"', "'", ":", ";", "-", "_", "/", " ", "<", ">", "@", "$", "%", "&", "#", "^", "*"];
+const dupeRegularCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", ",", "?", "!", "(", ")", '"', "'", ":", ";", "-", "_", "/", " ", "<", ">", "@", "$", "%", "&", "#", "^", "*"];
+
+const specialCharacters = ["a", "e", "i", "n", "o", "r", " "];
+const specialCharactersReplacement = ["@", "$", "%", "&", "#", "^", "*"];
+//a, e, i, n, o, r, space
+//"@", "$", "%", "&", "#", "^", "*"
 
 const regex = /^[a-z0-9.,?!();"':\/\-_ <>]+$/i;
+const cipherRegex = /^[a-z0-9.,?!();"':\/\-@$%&#^*_ <>]+$/i;
 
 async function encode() {
 	document.getElementById("sub-table-error").style.display = "none";
+	document.getElementById("missing-plaintext-error").style.display = "none";
 	const input = document.getElementById("plaintext-input").value;
 	const output = document.getElementById("ciphertext-input");
 	//const subTable = shuffleArray(regularCharacters);
 	const subTableInput = document.getElementById("sub-table-input");
 	let subTable = [];
+
+	if (input == "") {
+		document.getElementById("missing-plaintext-error").style.display = "block";
+		return;
+	}
 
 	if (subTableInput.value == "") {
 		subTable = shuffleArray(regularCharacters);
@@ -66,29 +79,40 @@ async function encode() {
 	let char;
 	let num;
 	let extraChar;
+	let specialCharactersNum;
+	let extraShit;
 
 	for (let i = 0; i < inputArray.length; i++) {
+		extraShit = "1";
 		char = inputArray[i];
 		num = dupeRegularCharacters.indexOf(char);
-		
-		if (i != 0 && char == inputArray[i - 1] && chanceOfTrue(35)) {
-			console.log("repeat " + char);
-			extraChar = subTable[dupeRegularCharacters.indexOf(">")];
-			console.log(dupeRegularCharacters.indexOf(">"));
-			console.log(extraChar);
+		outputChar = subTable[num];
 
-			outputArray.push(subTable[num] + extraChar);
-		} else if (chanceOfTrue(10)) {
-			console.log("add and delete " + char);
-			extraChar = subTable[Math.floor(Math.random() * (subTable.length - 1))] + subTable[dupeRegularCharacters.indexOf("<")];
-			console.log(extraChar);
-
-			outputArray.push(subTable[num] + extraChar);
-		} else {
-			console.log(char);
-			outputArray.push(subTable[num]);
+		if (specialCharacters.includes(char) && chanceOfTrue(50)) {
+			specialCharactersNum = specialCharacters.indexOf(char);
+			charNum = specialCharactersNum + 52;
+			outputChar = subTable[charNum];
+			console.log(specialCharactersNum);
+			console.log(subTable[54]);
+			console.log("substitute common letter");
+			extraShit = "0";
 		}
+
+		if (char == inputArray[i - 1] && chanceOfTrue(33) && extraShit == 1) { 
+			outputArray.push(subTable[dupeRegularCharacters.indexOf(">")]);
+			console.log("repeat prev. letter");
+		} else if (chanceOfTrue(10) && extraShit == 1) {
+			extraChar = subTable[Math.floor(Math.random() * (subTable.length - 1))] + subTable[dupeRegularCharacters.indexOf("<")];
+			outputArray.push(outputChar + extraChar);
+			console.log("add letter and then random letter and delete symbol");
+		} else {
+			outputArray.push(outputChar);
+			console.log("regular");
+		}
+		
 		output.value = outputArray.join("");
+		document.getElementById("encode-btn").disabled = false;
+		document.getElementById("decode-btn").disabled = false;
 	}
 }
 
@@ -122,7 +146,10 @@ function isValidSubstitutionTable(table) {
 }
 
 function decode() {
+	console.time();
 	document.getElementById("missing-table-error").style.display = "none";
+	document.getElementById("missing-ciphertext-error").style.display = "none";
+
 	const input = document.getElementById("ciphertext-input").value;
 	const output = document.getElementById("plaintext-input");
 	let outputArray = [];
@@ -130,6 +157,11 @@ function decode() {
 
 	const subTableInput = document.getElementById("sub-table-input");
 	let subTable;
+
+	if (input == "") {
+		document.getElementById("missing-ciphertext-error").style.display = "block";
+		return;
+	}
 
 	if (subTableInput.value == "") {
 		document.getElementById("missing-table-error").style.display = "block";
@@ -142,7 +174,7 @@ function decode() {
 		}
 	}
 
-	if (regex.test(input)) {
+	if (cipherRegex.test(input)) {
 		document.getElementById("ciphertext-error").style.display = "none";
 	} else {
 		document.getElementById("ciphertext-error").style.display = "block";
@@ -155,16 +187,49 @@ function decode() {
 	for (let i = 0; i < inputArray.length; i++) {
 		char = inputArray[i];
 		num = subTable.indexOf(char);
+		outputChar = dupeRegularCharacters[num];
 
-		if (dupeRegularCharacters[num] == "<") {
-			outputArray.pop();
-		} else if (dupeRegularCharacters[num] == ">") {
-			outputArray.push(outputArray[i - 1] + outputArray[i - 1]);
-		} else {
-			outputArray.push(dupeRegularCharacters[num]);
+		if (specialCharactersReplacement.includes(outputChar)) {
+			outputChar = specialCharacters[specialCharactersReplacement.indexOf(outputChar)];
 		}
-		console.log("input char: " + char + " output num: " + num + " output char: " + dupeRegularCharacters[num]);
+
+		if (outputChar == "<") {
+			outputArray.pop();
+		} else if (outputChar == ">") {
+			outputArray.push(outputArray[i - 1]);
+			console.log(outputArray[i - 1])
+		} else {
+			outputArray.push(outputChar);
+		}
+		//console.log("input char: " + char + " output num: " + num + " output char: " + dupeRegularCharacters[num]);
 	}
 
 	output.value = outputArray.join("");
+	document.getElementById("encode-btn").disabled = false;
+	document.getElementById("decode-btn").disabled = false;
+	console.timeEnd();
+}
+
+function disableBtn(id) {
+	const ciphertext = document.getElementById("ciphertext-input");
+	const plaintext = document.getElementById("plaintext-input");
+	if (id == 'cipher' && ciphertext.value == '') {
+		document.getElementById('decode-btn').disabled = true;
+	} else if (id == 'cipher' && ciphertext.value != '') {
+		document.getElementById('decode-btn').disabled = false;
+	}
+
+	if (id == "plain" && plaintext.value == "") {
+		document.getElementById("encode-btn").disabled = true;
+	} else if (id == "plain" && plaintext.value != "") {
+		document.getElementById("encode-btn").disabled = false;
+	}
+}
+
+function ctrlEnter(e, id) {
+	console.log(e.key);
+
+	if (e.key == "Enter" && e.key == "Control") {
+		console.log("yay");
+	}
 }
